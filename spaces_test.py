5a6b931f5b81a418e70361c3d4702b982bba35d2
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-07-15 15:01:55 +0000

    update container

--- spaces_test.py
+++ spaces_test.py
@@ -1,3 +1,18 @@
-import lightning as L
 import numpy as np
-import torch
+import pandas as pd
+import torchmetrics as tm
+import sklearn as sk
+import torch as t
+import pytorch_lightning as pl
+from rich import print as rprint
+import sys
+
+
+rprint("[bold green]collecting env installs[/bold green]", end="\n\n")
+print("python", sys.version)
+for lib in [np, pd, tm, sk, t, pl]:
+    print(
+        lib.__name__ + ": ",
+        lib.__version__,
+        "\n" if lib.__name__ == "pytorch_lightning" else "",
+    )
